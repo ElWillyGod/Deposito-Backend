ff7832a39f17170076ec239371ff75a2fe5a8c26
ElWillyGod <manuelantognazza@gmail.com>
2025-04-30 12:43:14 -0300

    controllers F1

--- Api/Controllers/UserController.cs
+++ Api/Controllers/UserController.cs
@@ -1,1 +1,29 @@
 using Core;
+using Core.Interfaces;
+
+
+namespace WebApi.Controllers
+{
+	[ApiController]
+	[Route("[Controllers]")]
+	public class UserController : ControllerBase
+	{
+		private readonly IUserService _userService;
+
+		public UserController(IUserService userService)
+		{
+			_userService = userService;
+		}
+		//esto solo deberia tirar un string
+		[HttpPost("user/login")]
+		public async Task<IActionResult> Authenticate([FromBody] User user)
+		{
+			var token = await _userService.AuthenticateUserAsync(user.Email, user.Password);
+			if (token == null)
+				return Unauthorized();
+
+			return Ok(token);
+		}
+
+	}
+}
